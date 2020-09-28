68be5e7046b1d690f8fb2622858bf44689e09f47
brennamaloy <bkm91466@creighton.edu>
2020-09-28 14:26:21 -0500

    master

--- forms/ifElse/ifElse.js
+++ forms/ifElse/ifElse.js
@@ -4,12 +4,13 @@ let userName = prompt("What is your name?")
 let stateName = prompt ("What state are you from?")
 let currentTemp = prompt ("What is the current temperature?")
 
+stateName = stateName.toUpperCase()
+uppercaseFirstLetter = userName.charAt(0).toUpperCase() + userName.slice(1);
 /*array statement*/
 let message = ["wear a warm coat, hat scarf and gloves.", "wear a warm coat but you won't need a hat scarf or gloves." , "wear a less heavy coat." , "wear a light jacket and smile!"]
 
 
 /*if else statement */
-
 if (stateName == 'NE' && currentTemp <32) {
   NSB.MsgBox(`Hi ${userName}, ${message[0]}`) 
 } else if (stateName == 'NE' && currentTemp >= 32 && currentTemp <= 50) {
@@ -17,7 +18,7 @@ if (stateName == 'NE' && currentTemp <32) {
 } else if (stateName == 'FL' && currentTemp >= 32 && currentTemp <= 50) {
   NSB.MsgBox(`Hi ${userName}, ${message[2]}`)
 } else if (stateName == 'FL' && currentTemp >= 50 && currentTemp <= 70) {
-  NSB.MsgBox(`Hi ${userName}, ${message[3]}`)
+  NSB.MsgBox(`Hi ${userName}, ${messae[3]}`)
 } else {
   NSB.MsgBox(`Hi ${userName}, please enter a valid response`)
 } 
--- forms/switch/switch.js
+++ forms/switch/switch.js
@@ -1,14 +1,17 @@
 
 /*define the variables */ 
-let userName = prompt("What is your name?")
-let stateName = prompt ("What state are you from?")
-let currentTemp = prompt ("What is the current temperature?")
+ userName = prompt("What is your name?")
+ stateName = prompt ("What state are you from?")
+currentTemp = prompt ("What is the current temperature?")
+
+stateName = stateName.toUpperCase()
+FirstLetter = userName.charAt(0).toUpperCase() + userName.slice(1);
 
 /*array statement*/
-let message = ["wear a warm coat, hat scarf and gloves.", "wear a warm coat but you won't need a hat scarf or gloves." , "wear a less heavy coat." , "wear a light jacket and smile!"]
+message = ["wear a warm coat, hat scarf and gloves.", "wear a warm coat but you won't need a hat scarf or gloves." , "wear a less heavy coat." , "wear a light jacket and smile!"]
 
 /*switch statement*/ 
-switch (true) {
+switch(true) {
 case (stateName == "NE" && currentTemp < 32):
   NSB.MsgBox(`Hi ${userName}, ${message[0]}`)
   break
@@ -24,6 +27,3 @@ case (stateName = "FL" && currentTemp >=50 && currentTemp <=70):
 default:
   NSB.MsgBox(`Hello ${userName}, please enter a valid response.`)
 }
-
-  
-  
